447d9eb26e672402487042281496e5847ba6fdff
darekb <kontakt@3wm.pl>
2017-02-17 16:45:49 +0100

    wip

--- main.c
+++ main.c
@@ -22,12 +22,16 @@ uint8_t buflen = VW_MAX_MESSAGE_LEN;
 char wiad[VW_MAX_MESSAGE_LEN] = "";
 uint8_t i;
 volatile uint8_t stage = 0;
+volatile uint8_t counter = 0;
+volatile uint8_t watchdogResetCount = 0;
 
 void resetBuffer(char *buffer);
 
 void sendDataToUART(char *buffer);
 
 uint8_t checkEndForData(char *str);
+
+void resetWatchDog(void);
 
 void stage0_listenRX();
 
@@ -36,6 +40,9 @@ void stage1_collectData(char *buffer);
 uint8_t stage2_getTempFromDS18B20(char *buffer);
 
 int main(void) {
+  TCCR0B |= (1 << CS02) | (1 << CS00);//prescaler 1024
+  TIMSK0 |= (1 << TOIE0);//przerwanie przy przepłnieniu timera0
+
   wdt_enable(WDTO_8S);
 
   //init UART transmition
@@ -92,11 +99,16 @@ void resetBuffer(char *buffer) {
 void sendDataToUART(char *buffer) {
   //slUART_WriteString("\r\n");
   slUART_WriteString(buffer);
-  slUART_WriteString("\r\n");
+  slUART_WriteString("\n");
 }
 
 uint8_t checkEndForData(char *str) {
   return strchr(str, 'z') ? 1 : 0;
+}
+
+void resetWatchDog(void){
+  wdt_reset();
+  watchdogResetCount = 0;
 }
 
 void stage0_listenRX() {
@@ -114,19 +126,20 @@ void stage1_collectData(char *buffer) {
   }
   sei();
   if (checkEndForData(buffer)) {
+    resetWatchDog();
     sendDataToUART(buffer);
     stage = 2;
   } else {
+    slUART_WriteString("errorz\n");
     stage = 0;
   }
 }
 
 uint8_t stage2_getTempFromDS18B20(char *buffer) {
   _delay_ms(3000);
-  wdt_reset();
+  resetWatchDog();
   resetBuffer(buffer);
   if (slDS18B20_ReturnTemp(buffer)) {
-
 #if showDebugDataMain == 1
     slUART_WriteString("Error get temperature from slDS18B20\r\n");
 #endif
@@ -136,4 +149,16 @@ uint8_t stage2_getTempFromDS18B20(char *buffer) {
   sendDataToUART(buffer);
   stage = 0;
   return 0;
+}
+
+ISR(TIMER0_OVF_vect) {
+  //co 0.01632sek.
+  counter = counter + 1;
+  if (counter == 450) {//7.344 sek. dla 16MHz have to less than 8s (watchdog WDTO_8S)
+    counter = 0;
+    watchdogResetCount = watchdogResetCount + 1;
+    if(watchdogResetCount <= 9){ //9 * 7.344 = 66.096 sek. about 2 measures
+      wdt_reset();
+    }
+  }
 }
